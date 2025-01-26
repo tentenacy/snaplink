35581bf816adf4371efe5f21779a8817d62666f0
tenacy <tentenacy@gmail.com>
2025-01-26 13:00:55 +0900

    프로젝트 생성

---
+++ src/test/java/com/tenacy/snaplink/SnaplinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenacy.snaplink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnaplinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
