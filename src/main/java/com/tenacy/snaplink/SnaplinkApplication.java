1abf01a785113522c59b13f5f516c21a68847287
tenacy <tentenacy@gmail.com>
2025-08-07 17:29:45 +0900

    프로젝트 생성

---
+++ src/main/java/com/tenacy/snaplink/SnaplinkApplication.java
@@ -0,0 +1,13 @@
+package com.tenacy.snaplink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnaplinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnaplinkApplication.class, args);
+	}
+
+}
