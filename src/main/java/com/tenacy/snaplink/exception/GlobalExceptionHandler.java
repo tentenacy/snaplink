e574ef4413cef394e9b04287e350c97f3eefb5c2
tenacy <tentenacy@gmail.com>
2025-08-08 02:03:24 +0900

    chore: CI/CD 파이프라인 생성

--- src/main/java/com/tenacy/snaplink/exception/GlobalExceptionHandler.java
+++ src/main/java/com/tenacy/snaplink/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import io.micrometer.core.instrument.Counter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,12 +13,24 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
     private final Counter errorCounter;
 
+    /**
+     * @Validated로 검증 시 binding 못하는 경우
+     * CustomCollectionValidator로 검증 시 binding 못하는 경우
+     */
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<ErrorResponse> handleBindException(final BindException ex) {
+        errorCounter.increment();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ErrorResponse.of("Invalid input", HttpStatus.BAD_REQUEST.value(), ex.getBindingResult()));
+    }
+
     @ExceptionHandler(UrlNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleUrlNotFound(UrlNotFoundException ex) {
         errorCounter.increment();
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(new ErrorResponse(ex.getMessage()));
+                .body(new ErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND.value()));
     }
 
     @ExceptionHandler({DuplicateShortCodeException.class, UrlExpiredException.class})
@@ -25,7 +38,7 @@ public class GlobalExceptionHandler {
         errorCounter.increment();
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(new ErrorResponse(ex.getMessage()));
+                .body(new ErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST.value()));
     }
 
     @ExceptionHandler(Exception.class)
@@ -33,6 +46,7 @@ public class GlobalExceptionHandler {
         errorCounter.increment();
 
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new ErrorResponse("An unexpected error occurred"));
+                .body(new ErrorResponse("An unexpected error occurred",
+                        HttpStatus.INTERNAL_SERVER_ERROR.value()));
     }
 }
